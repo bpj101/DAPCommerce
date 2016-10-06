6d6119baca87e893dd4e4caa9961969058dfa128
Paul Julian <bpj101@dameagnesproductions.com>
2016-10-05 21:36:54 -0700

    Added Paginations to main page

--- api/api.js
+++ api/api.js
@@ -1,38 +1,48 @@
 'use strict';
 const
-    routes = require('express').Router(),
-    async = require('async'),
-    faker = require('faker'),
-    Category = require('../models/category'),
-    Product = require('../models/product');
+  routes = require('express').Router(),
+  async = require('async'),
+  faker = require('faker'),
+  Category = require('../models/category'),
+  Product = require('../models/product');
+
+routes.post('/search', (req, res, next) => {
+  Product.search({
+    query_string: {
+      query: req.body.search_term
+    }
+  }, (err, results) => {
+    res.json(results);
+  });
+});
 
 routes.get('/:name', function(req, res, next) {
-    var product;
-    async.waterfall([
+  var product;
+  async.waterfall([
 
-        function(callback) {
-            Category.findOne({
-                name: req.params.name
-            }, function(err, category) {
-                if (err) return next(err);
-                callback(null, category);
-            });
-        },
-        function(category, callback) {
-            for (var i = 0; i < 30; i++) {
-                product = new Product();
-                product.category = category._id;
-                product.name = faker.commerce.productName();
-                product.price = faker.commerce.price();
-                product.image = faker.image.image();
-                console.log(product.name);
-                product.save();
-            }
-        }
-    ]);
-    res.send({
-        message: 'Success'
-    });
+    function(callback) {
+      Category.findOne({
+        name: req.params.name
+      }, function(err, category) {
+        if (err) return next(err);
+        callback(null, category);
+      });
+    },
+    function(category, callback) {
+      for (var i = 0; i < 30; i++) {
+        product = new Product();
+        product.category = category._id;
+        product.name = faker.commerce.productName();
+        product.price = faker.commerce.price();
+        product.image = faker.image.image();
+        console.log(product.name);
+        product.save();
+      }
+    }
+  ]);
+  res.send({
+    message: 'Success'
+  });
 });
 
 
--- routes/main.js
+++ routes/main.js
@@ -1,102 +1,141 @@
 'use strict';
 
 const
-    routes = require('express').Router(),
-    User = require('../models/user'),
-    Product = require('../models/product');
+  routes = require('express').Router(),
+  User = require('../models/user'),
+  Product = require('../models/product');
+
+function paginate(req, res, next) {
+  let perPage = 8;
+  let page = req.params.page;
+
+  Product
+    .find({})
+    .skip(perPage * page)
+    .limit(perPage)
+    .populate('category')
+    .exec((err, products) => {
+      if (err) return next(err);
+      Product.count().exec((err, count) => {
+        if (err) return next(err);
+        res.render('product-main', {
+          message: req.flash('msg'),
+          products: products,
+          pages: count / perPage
+        });
+      });
+    });
+}
 
 Product.createMapping((err, mapping) => {
-    if (err) {
-        console.log('error creating mapping');
-        console.log(err);
-    } else {
-        console.log('Mapping Created');
-        console.log(mapping);
-    }
+  if (err) {
+    console.log('error creating mapping');
+    console.log(err);
+  } else {
+    console.log('Mapping Created');
+    console.log(mapping);
+  }
 });
 
 let stream = Product.synchronize();
 let count = 0;
 
 stream.on('data', () => {
-    count++;
+  count++;
 });
 
 stream.on('close', () => {
-    console.log('Indexed ' + count + ' documents');
+  console.log('Indexed ' + count + ' documents');
 });
 
 stream.on('error', (err) => {
-    console.log(err);
+  console.log(err);
 });
 
 // Routes
 
 routes.post('/search', (req, res, next) => {
-    // additional coding
-    res.redirect('/search?q=' + req.body.q);
+  // additional coding
+  res.redirect('/search?q=' + req.body.q);
 });
 
 routes.get('/search', (req, res, next) => {
-    if (req.query.q) {
-        Product.search({
-            query_string: {
-                query: req.query.q
-            },
-        }, (err, results) => {
-            if (err) return next(err);
-            let data = results.hits.hits.map((hit) => {
-                return hit;
-            });
-            console.log(data);
-            res.render('search-result', {
-                query: req.query.q,
-                data: data
-            });
-        });
-    }
+  if (req.query.q) {
+    Product.search({
+      query_string: {
+        query: req.query.q
+      }
+    }, {
+      hydrate: true
+    }, (err, results) => {
+      if (err) return next(err);
+      let xdata = [];
+      let data = results.hits.hits.map((hit) => {
+        xdata.push(hit);
+        return hit;
+      });
+      console.log(xdata);
+      res.render('search-result', {
+        query: req.query.q,
+        data: xdata
+      });
+
+    });
+  }
 });
 
 
 routes.get('/', function(req, res, next) {
+
+  if (req.user) {
+    paginate(req, res, next);
+
+  } else {
     res.render('main', {
-        message: req.flash('msg'),
-        user: req.user
+      message: req.flash('msg'),
+      user: req.user
     });
+  }
+});
+
+routes.get('/page/:page', (req, res, next) => {
+
+  paginate(req, res, next);
+
 });
 
 routes.get('/about', function(req, res, next) {
-    res.render('about', {
-        message: req.flash('msg')
-    });
+  res.render('about', {
+    message: req.flash('msg')
+  });
 });
 
 routes.get('/products/:id', (req, res, next) => {
-    Product
-        .find({
-            category: req.params.id
-        })
-        .populate('category')
-        .exec((err, products) => {
-            if (err) return next(err);
-            res.render('category', {
-                products: products
-            });
-        });
+  Product
+    .find({
+      category: req.params.id
+    })
+    .populate('category')
+    .exec((err, products) => {
+      if (err) return next(err);
+      res.render('category', {
+        products: products
+      });
+    });
 });
 
 routes.get('/product/:id', (req, res, next) => {
-    Product
-        .findById({
-            _id: req.params.id
-        })
-        .populate('category')
-        .exec((err, product) => {
-            if (err) return next(err);
-            res.render('product', {
-                product: product
-            });
-        });
+  Product
+    .findById({
+      _id: req.params.id
+    })
+    .populate('category')
+    .exec((err, product) => {
+      if (err) return next(err);
+      res.render('product', {
+        product: product
+      });
+    });
 });
 
 module.exports = routes;
